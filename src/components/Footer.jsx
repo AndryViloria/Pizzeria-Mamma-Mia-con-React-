823043951b547ebec47a38445ea882153b387569
Andry Viloria <andryviloria2@gmail.com>
2022-11-12 18:02:01 -0400

    Subiendo proyecto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Desafío: Pizzería Mamma Mia - React. Elaborado por Andry Viloria. Desafío Latam 2022.</p>
+        </footer>
+    )
+}
+
+export default Footer
